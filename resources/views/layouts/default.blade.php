aaedacad38882d4cf98f6ae5622934256ca0c8fb
zhaohongwu <401032639@qq.com>
2019-04-23 05:18:11 +0800

    用户注册表单

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-@yield('content')
+@include('layouts._header')
+<div class="container">
+  <div class="offset-md-1 col-md-10">
+    @include('shared._messages')
+    @yield('content')
+    @include('layouts._footer')
+  </div>
+</div>
+
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
